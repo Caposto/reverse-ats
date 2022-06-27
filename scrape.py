59c52a73dd5c5a70d0b0b86889977cf465b15576
ChristianA <christiand.apostol@outlook.com>
2022-06-26 20:54:57 -0400

    installed libraries and parsers

--- scrape.py
+++ scrape.py
@@ -1,1 +1,18 @@
-print("Hello")
+import requests 
+import pandas as pd
+from bs4 import BeautifulSoup
+
+# lxml parsers
+#$ apt-get install python-lxml
+#$ easy_install lxml
+#$ pip install lxml
+
+#HTML 5 web parser - the way browsers do it
+#$ apt-get install python-html5lib
+#$ easy_install html5lib
+#$ pip install html5lib
+
+# Create a BeautifulSoup object
+soup = BeautifulSoup('<b class="boldest">Extremely bold</b>', 'lxml')
+tag = soup.b
+print(type(tag))
