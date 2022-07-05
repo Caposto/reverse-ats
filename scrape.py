44c7b1536cf0ed4923701ed15da5be60eff694aa
ChristianA <christiand.apostol@outlook.com>
2022-07-04 23:10:59 -0400

    scraped description

--- scrape.py
+++ scrape.py
@@ -14,11 +14,18 @@ from bs4 import BeautifulSoup
 
 adobe_url = "https://www.linkedin.com/jobs/view/3114502964/?alternateChannel=search&refId=zFDuw0gRwxQIow6TJ5rl%2Bw%3D%3D&trackingId=MQvaNALJ6NJH6kOv0d0FGw%3D%3D"
 
-data = requests.get(adobe_url)
-
 # Write results to an html file to make it easier to read
-with open("data.html", "w", encoding="utf-8") as f:
-    f.write(data.text)
+#with open("data.html", "w", encoding="utf-8") as f:
+#    f.write(data.text)
 
-soup = BeautifulSoup(data.text, 'lxml')
-print("Job Title: " + soup.title.string)
+def getJobDescription(post_url):
+    # Request the html
+    data = requests.get(post_url)
+
+    # Create beautiful soup object
+    soup = BeautifulSoup(data.text, 'lxml')
+    tags = soup.find_all("script")
+    # print("Job Title: " + soup.title.string)
+    print(tags[1].text)
+
+getJobDescription(adobe_url)
