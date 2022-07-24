05930e6405a9b4677c2cb4799c1e82d22b932da7
ChristianA <christiand.apostol@outlook.com>
2022-07-23 21:59:47 -0400

    first classification attempt

--- Post.py
+++ Post.py
@@ -1,33 +1,53 @@
-import requests
-from bs4 import BeautifulSoup
-import json
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+import time
 
 class Post:
 
-    # Initialize the job URL and create the Soup object to pass to methods
+    # Initalize post object by passing in URL and a global webdriver
     def __init__(self, job_url):
-        self.req = requests.request("GET", job_url, timeout=1)
-
-        # Create soup object if the status code went through, else Print Error
-        if self.req.status_code == 200:
-            self.soup = BeautifulSoup(self.req.text, 'lxml')
-            print("Success!")
-        else:
-            print("Error with request: " + str(self.data.status_code))
+        # Create Webdriver
+        options = Options()
+        options.add_argument("start-maximized")
+        options.add_experimental_option("detach", True)
+        wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        self.wd = wd
+        self.job_url = job_url
     
-    def getJobDescription(self):
-        tag = self.soup.find(id="job-details")
-        print(tag)
+    def get_job_description(self):
+        more_button = self.wd.find_element(By.CLASS_NAME, "show-more-less-html__button")
+        self.wd.execute_script("arguments[0].click();", more_button)
+        description_tag = self.wd.find_element(By.CLASS_NAME, "show-more-less-html__markup")
+        return description_tag.text
+
+    def get_job_title(self):
+        title_tag = self.wd.find_element(By.CLASS_NAME, "top-card-layout__title font-sans text-lg papabear:text-xl font-bold leading-open text-color-text mb-0 topcard__title")
+        return title_tag.text
+
+    def get_employer(self):
+        employer_tag = self.wd.find_element(By.CLASS_NAME, "topcard__org-name-link topcard__flavor--black-link")
+        return employer_tag.text
+
+    def write_to_file(self, filename, text):
+        return
+
+    def proccess_request(self):
+        time.sleep(3)
+        self.wd.quit()
 
 
-# Run the following code to write job_description to a file
-# def formatStringToMultipleLines():
 
-# print(getJobDescription(adobe_url))
-
-# Run this to update description.txt with the current request's data
-# copyDescription(adobe_url)
-
-# Write results to an html file to make it easier to read
-#with open("data.html", "w", encoding="utf-8") as f:
-#    f.write(data.text)
+# Function for writing job description to a text file
+# Commas and other characters are filtered out & each character is written on its own line
+# filename (string): name of destination text file
+# text (string): description string (usually in the form of a webdriver WebElement.text)
+def write_to_file(filename, text):
+    new_text = text.replace(',', '').replace(':', '')
+    words = new_text.split()
+    with open(filename, 'w') as f:
+        for word in words:
+            f.write(word + '\n')
--- scrape.py
+++ scrape.py
@@ -1,9 +1,7 @@
-from asyncore import write
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 
 import time
 import requests 
@@ -14,38 +12,21 @@ from Post import Post
 # Test URLs
 harris_url = "https://www.linkedin.com/jobs/view/3177003033/?alternateChannel=search&refId=NZT9zV8uEoIg8agLGIavuA%3D%3D&trackingId=l23qk%2BcE7A9qdnEcH6JbWw%3D%3D"
 
-# Setup Chrome Web Driver
-# CHROME_DRIVER_PATH = './chromedriver.exe'
+# options = Options()
+# options.add_argument("start-maximized")
+# options.add_experimental_option("detach", True)
 
-options = Options()
-options.add_argument("start-maximized")
-options.add_experimental_option("detach", True)
-
-wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-wd.get(harris_url)
+# wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+# wd.get(harris_url)
 
 # Scroll to bottom of page - hopefully this exposes all HTML
 # wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-more_button = wd.find_element(By.CLASS_NAME, "show-more-less-html__button")
-wd.execute_script("arguments[0].click();", more_button)
-div = wd.find_element(By.CLASS_NAME, "show-more-less-html__markup")
+harris_job = Post(harris_url)
+print(harris_job.get_job_description)
+# print(harris_job.get_employer())
 
-# Function for writing job description to a text file
-# Commas and other characters are filtered out & each character is written on its own line
-# filename (string): name of destination text file
-# text (string): description string (usually in the form of a webdriver WebElement.text)
-def write_to_file(filename, text):
-    new_text = text.replace(',', '').replace(':', '')
-    words = new_text.split()
-    with open(filename, 'w') as f:
-        for word in words:
-            f.write(word + '\n')
 
-write_to_file('description.txt', div.text)
-
-time.sleep(3)
-wd.quit()
 
 
 
