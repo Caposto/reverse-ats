d2932215d4b8f725cfc16e20d38172ddb64b713f
ChristianA <christiand.apostol@outlook.com>
2022-07-25 23:23:20 -0400

    refactoring Post class and adding comments

--- Post.py
+++ Post.py
@@ -2,34 +2,65 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-# FIXME: add methods for Seniority level, employment type, job function, and industries
-class Post:
-    # Create and assign Web Driver and run Selenium browser on the given URL
+# A class for wrapping Selenium web scraping functionality into simple methods
+# Scrapes data from the LinkedIn Job Board
+class Job_Post:
+
+    # FIXME: Test against Global WebDriver
     def __init__(self, job_url) -> None:
+        """Creates Chromedriver object and runs it on the given job URL"""
         try:
-            # Create Webdriver
             options = Options()
             options.add_argument("start-maximized")
-            options.add_experimental_option("detach", True)
+            options.add_experimental_option("detach", True) # Prevent browser from closing immediately
             wd = webdriver.Chrome(options=options)
+
             self.wd = wd
             self.job_url = job_url
             self.wd.get(self.job_url)
+        # FIXME: Research if there are better ways/best practices for handling error and exceptions
         except Exception as e:
             return "Error creating the web driver " + e
 
     def __str__(self) -> str:
-        info = self.get_info_list()
-
-        return("POSITION: " + self.get_job_title() + "\n" +
-               "EMPLOYER: " + self.get_employer() + "\n" +
-               "DESCRIPTION: " + self.get_job_description() + "\n" +
+        info = self.scrape_info_list()
+        return("POSITION: " + self.scrape_job_title() + "\n" +
+               "EMPLOYER: " + self.scrape_employer() + "\n" +
+               "DESCRIPTION: " + self.scrape_raw_description() + "\n" +
                "SENIORITY LEVEL: " + info['Seniority level'] + "\n" +
                "EMPLOYMENT TYPE: " + info['Employment type'] + "\n" +
                "JOB FUNCTION: " + info['Job function'] + "\n" +
-               "INDUSTRIES: " + info['Industries']) 
+               "INDUSTRIES: " + info['Industries'])
     
-    def get_job_description(self) -> str:
+    # UTILITY METHODS
+
+    def get_url(self) -> str:
+        return self.job_url
+
+    def end_session(self) -> None:
+        self.wd.quit()
+
+    def expose_description(self) -> None:
+        """Helper function that uses Selenium to press button that reveals entire job description"""
+        try: 
+            more_button = self.wd.find_element(By.CLASS_NAME, "show-more-less-html__button")
+            self.wd.execute_script("arguments[0].click();", more_button)
+        except Exception as e:
+            return "Error finding button: " + e
+
+    # FIXME: Implement
+    def write_to_file(self, filename, text):
+        return
+    
+    # SCRAPING METHODS
+
+    # FIXME: Implement
+    def get_job_id(self):
+        return 
+
+    def scrape_raw_description(self) -> str:
+        """Calls expose_desciption() to access the proper HTML elements and then returns all of the text contained within the job description"""
+        # FIXME: Try & Except or "if tag:"
         try:
             self.expose_description()
             description_tag = self.wd.find_element(By.CLASS_NAME, "show-more-less-html__markup")
@@ -37,32 +68,28 @@ class Post:
         except Exception as e:
             return "Error scraping the job description: " + e
 
-    def get_job_title(self) -> str:
+    def scrape_job_title(self) -> str:
         try:
             title_tag = self.wd.find_element(By.CLASS_NAME, "top-card-layout__title")
             return title_tag.text
         except Exception as e:
             return "Error scraping the job title: " + e
 
-    def get_employer(self) -> str:
+    def scrape_employer(self) -> str:
         try:
             employer_tag = self.wd.find_element(By.CLASS_NAME, "topcard__org-name-link")
             return employer_tag.text
         except Exception as e:
             return "Error scraping the employer: " + e
-    
-    # Helper function that automates browser to expand the section of the web page that contains the job description
-    def expose_description(self) -> None:
-        try: 
-            more_button = self.wd.find_element(By.CLASS_NAME, "show-more-less-html__button")
-            self.wd.execute_script("arguments[0].click();", more_button)
-        except Exception as e:
-            return "Error finding button: " + e
 
-    def end_session(self) -> None:
-        self.wd.quit()
-
-    def get_info_list(self) -> str:
+    def scrape_info_list(self) -> dict:
+        """
+        Scrapes additional details about the job description and retrns the information in a dictionary
+        Seniority Level (Junior, Senior....)
+        Employment Type (Full-Time, Part-Time, Internship....)
+        Job Function (Department within company)
+        Industry
+        """
         job_info_list = self.wd.find_element(By.CLASS_NAME, "description__job-criteria-list")
         info = {}
         
@@ -76,9 +103,6 @@ class Post:
                 info[c] = t
 
         return info
-
-    def write_to_file(self, filename, text):
-        return
 
 
 # Function for writing job description to a text file
--- scrape.py
+++ scrape.py
@@ -1,4 +1,4 @@
-from Post import Post
+from Post import Job_Post
 
 # Test URLs
 harris_url = "https://www.linkedin.com/jobs/view/3177003033/?alternateChannel=search&refId=NZT9zV8uEoIg8agLGIavuA%3D%3D&trackingId=l23qk%2BcE7A9qdnEcH6JbWw%3D%3D"
@@ -6,11 +6,11 @@ ey_url = "https://www.linkedin.com/jobs/view/3178858633/?alternateChannel=search
 
 # FIXME: How do I know if the session has gotten all of the information it needs?
 # FIXME: What are some alternatives to manual time.sleep() times
-harris_job = Post(harris_url)
+harris_job = Job_Post(harris_url)
 print(harris_job)
 # harris_job.end_session()
 
-# ey_job = Post(ey_url)
+# ey_job = Job_Post(ey_url)
 # print(ey_job)
 # ey_job.end_session()
 
