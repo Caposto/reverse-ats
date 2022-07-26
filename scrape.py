e6d66d4fb80e35f7d39d54d0d7d6cddc6cd86929
ChristianA <christiand.apostol@outlook.com>
2022-07-26 15:37:54 -0400

    updated test postings

--- scrape.py
+++ scrape.py
@@ -1,14 +1,19 @@
 from Post import Job_Post
+from test_jobs import job_postings
 
 # Test URLs
 harris_url = "https://www.linkedin.com/jobs/view/3177003033/?alternateChannel=search&refId=NZT9zV8uEoIg8agLGIavuA%3D%3D&trackingId=l23qk%2BcE7A9qdnEcH6JbWw%3D%3D"
-ey_url = "https://www.linkedin.com/jobs/view/3178858633/?alternateChannel=search&refId=HSrPEUNyGlfqaMRMwcELlw%3D%3D&trackingId=YmvlBHj9gQxpxxAQNm4AxQ%3D%3D"
+ey_url = "https://www.linkedin.com/jobs/view/3148171398/?alternateChannel=search&refId=rL4qxZB4xgCbtk2ac4s8Hg%3D%3D&trackingId=p39dPQzT8UqeUBlTgFiTbQ%3D%3D"
 
 # FIXME: How do I know if the session has gotten all of the information it needs?
 # FIXME: What are some alternatives to manual time.sleep() times
-harris_job = Job_Post(harris_url)
-print(harris_job)
+# harris_job = Job_Post(harris_url)
+# print(harris_job)
 # harris_job.end_session()
+
+for job in job_postings:
+    url = job_postings[job]["url"]
+    j = Job_Post(url)
 
 # ey_job = Job_Post(ey_url)
 # print(ey_job)
