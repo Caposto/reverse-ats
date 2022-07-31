7083a3111f11a47846f02b1cca5627627a1194b4
ChristianA (PC) <christiand.apostol@outlook.com>
2022-07-31 18:02:50 -0400

    NoSuchElementException handling

--- Post.py
+++ Post.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 # Exceptions
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
 
 # A class for wrapping Selenium web scraping functionality into simple methods
 # Scrapes data from the LinkedIn Job Board
@@ -14,13 +15,13 @@ class Job_Post:
         Initalize and retrieve Job Posting with Selenium ChromeDriver
         Params: job_url - url of job posting, webdriver - globally declared Chromedriver
         """
-        self.wd = webdriver
-        self.job_url = job_url
+        self._wd = webdriver
+        self.__job_url = job_url
         try:
-            self.wd.get(self.job_url)
+            self._wd.get(self.__job_url)
         except Exception as e:
             print("Exception raised when launching browser: " + str(e))
-            self.wd.close()
+            self._wd.close()
 
     def __str__(self) -> str:
         info = self.scrape_info_list()
@@ -33,9 +34,8 @@ class Job_Post:
                "INDUSTRIES: " + info['Industries'])
     
     # UTILITY METHODS
-    # FIXME: Implement public, private, and protected keywords as necessary
     def end_session(self):
-        self.close()
+        self._wd.close()
 
     def write_to_file(self, filename):
         """
@@ -58,11 +58,12 @@ class Job_Post:
         '?alternateChannel=search&refId=NZT9zV8uEoIg8agLGIavuA%3D%3D&trackingId=l23qk%2BcE7A9qdnEcH6JbWw%3D%3D']
         The ID is the 6th element
         """
-        url_array = self.job_url.split("/")
-        if url_array[2] == 'www.linkedin.com':
-            id = self.job_url.split("/")[5]
+        url_array = self.__job_url.split("/")
+        if url_array[2] == 'www.linkedin.com' and url_array:
+            id = url_array[5]
             return id
-        return "This site is not compatible with the webscraper"
+        else: # incorrect URL format or different site entirely
+            return "The given url is not compatible with the webscraper"
 
     def scrape_raw_description(self, time=5) -> str:
         """
@@ -70,33 +71,33 @@ class Job_Post:
         Params: time = seconds before wait ex
         Throws TimeoutException if the element is not found in the specified time
         """
-        more_button = WebDriverWait(self.wd, time).until(EC.presence_of_element_located(
+        # FIXME: Implement explicit waits on other scraping methods as needed
+        more_button = WebDriverWait(self._wd, time).until(EC.presence_of_element_located(
                                                       (By.CLASS_NAME, "show-more-less-html__button")))
         if (more_button):
-            self.wd.execute_script("arguments[0].click();", more_button)
-            description_tag = self.wd.find_element(By.CLASS_NAME, "show-more-less-html__markup") 
-            return description_tag.text
+            self._wd.execute_script("arguments[0].click();", more_button)
+            try:
+                description_tag = self._wd.find_element(By.CLASS_NAME, "show-more-less-html__markup") 
+                return description_tag.text
+            except NoSuchElementException as e:
+                return "Job Description Not Found, NoSuchElementException raised: " + str(e)
         else:
-            self.wd.close()
+            self._wd.close()
             return "Error Finding More Button"
-        # FIXME: Does the above logic work or does the TimeoutException need to be addressed?
-        #except TimeoutException as ex:
-        #    print("Exception Thrown: " + str(ex))
-        #    self.wd.close()
 
     def scrape_job_title(self) -> str:
         try:
-            title_tag = self.wd.find_element(By.CLASS_NAME, "top-card-layout__title")
+            title_tag = self._wd.find_element(By.CLASS_NAME, "top-card-layout__titles")
             return title_tag.text
-        except Exception as e:
-            return "Error scraping the job title: " + e
+        except NoSuchElementException as e:
+            return "Job Title Not Found, NoSuchElementException raised: " + str(e)
 
     def scrape_employer(self) -> str:
         try:
-            employer_tag = self.wd.find_element(By.CLASS_NAME, "topcard__org-name-link")
+            employer_tag = self._wd.find_element(By.CLASS_NAME, "topcard__org-name-link")
             return employer_tag.text
-        except Exception as e:
-            return "Error scraping the employer: " + e
+        except NoSuchElementException as e:
+            return "Employer Not Found, NoSuchElementException raised: " + str(e)
 
     def scrape_info_list(self) -> dict:
         """
@@ -106,16 +107,20 @@ class Job_Post:
         Job Function (Department within company)
         Industry
         """
-        job_info_list = self.wd.find_element(By.CLASS_NAME, "description__job-criteria-list")
-        info = {}
+        try:
+            job_info_list = self._wd.find_element(By.CLASS_NAME, "description__job-criteria-list")
+            info = {}
         
-        if job_info_list:
-            categories = job_info_list.find_elements(By.TAG_NAME, "h3")
-            types = job_info_list.find_elements(By.TAG_NAME, "span")
+            if job_info_list:
+                categories = job_info_list.find_elements(By.TAG_NAME, "h3")
+                types = job_info_list.find_elements(By.TAG_NAME, "span")
 
-            for i in range(len(categories)):
-                c = categories[i].text
-                t = types[i].text
-                info[c] = t
+                for i in range(len(categories)):
+                    c = categories[i].text
+                    t = types[i].text
+                    info[c] = t
 
-        return info
+            return info
+
+        except NoSuchElementException as e:
+            return "Additional Job Details Not Found, NoSuchElementException raised: " + str(e)
--- scrape.py
+++ scrape.py
@@ -1,9 +1,10 @@
-from Post import Job_Post
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from Post import Job_Post
 
 options = Options()
+# FIXME: How do I run the webdriver without having a separate window pop up?
+# options.add_argument('headless')
 options.add_argument("start-maximized")
 options.add_experimental_option("detach", True) # Prevent browser from closing immediately
 wd = webdriver.Chrome(options=options)
@@ -14,8 +15,10 @@ ey_url = "https://www.linkedin.com/jobs/view/3148171398/?alternateChannel=search
 
 # FIXME: How do I know if the session has gotten all of the information it needs?
 harris_job = Job_Post(harris_url, wd)
-harris_job.write_to_file(harris_job.scrape_job_id() + ".txt")
+# harris_job.write_to_file(harris_job.scrape_job_id() + ".txt")
+print(harris_job.scrape_job_title())
 harris_job.end_session()
+
 # print(harris_job)
 
 
