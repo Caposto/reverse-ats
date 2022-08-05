9f839121e62b07d5b79065759b7bf8937da7ba0e
ChristianA (PC) <christiand.apostol@outlook.com>
2022-08-04 22:06:19 -0400

    testing pre-commit

--- scrape/__main__.py
+++ scrape/__main__.py
@@ -14,7 +14,7 @@ wd = webdriver.Chrome(options=options)
 harris_url = "https://www.linkedin.com/jobs/view/3177003033/?alternateChannel=search&refId=NZT9zV8uEoIg8agLGIavuA%3D%3D&trackingId=l23qk%2BcE7A9qdnEcH6JbWw%3D%3D"
 ey_url = "https://www.linkedin.com/jobs/view/3148171398/?alternateChannel=search&refId=rL4qxZB4xgCbtk2ac4s8Hg%3D%3D&trackingId=p39dPQzT8UqeUBlTgFiTbQ%3D%3D"
 
-
+# TESTING PRE-COMMIT
 if __name__ == "__main__":
     # FIXME: How do I know if the session has gotten all of the information it needs?
     job = Job_Post(job_postings["MLB"]["url"], wd)
