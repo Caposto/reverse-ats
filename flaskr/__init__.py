e9c42ef351f460bf2be7a0aa65007e425aadfb87
ChristianA (PC) <christiand.apostol@outlook.com>
2022-09-04 16:25:44 -0400

    extracted pdf text from resume

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,24 @@
+import PyPDF2
+import os
 from flask import Flask, render_template
 
 # Working with PDFs in FLASK: https://www.geeksforgeeks.org/working-with-pdf-files-in-python/
 
 app = Flask(__name__)
 
+parent_dir = os.path.join(os.getcwd(), 'flaskr')
+resume_path = os.path.join(parent_dir, "resume.pdf")
+
+pdf = open(resume_path, 'rb')
+
+pdf_reader = PyPDF2.PdfFileReader(pdf)
+
+page = pdf_reader.getPage(0)
+
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    pdf_text = page.extract_text()
+    return render_template("index.html", pdf_text=pdf_text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
