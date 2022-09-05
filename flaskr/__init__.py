f78f996a938c4329f55a8d0cf6cc9843ff308753
ChristianA (PC) <christiand.apostol@outlook.com>
2022-09-05 13:14:28 -0400

    used spacy to extract keywords from resume

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,6 @@
 import PyPDF2
 import os
-from flaskr.pdf import extract_text
+from flaskr.pdf import extract_text, extract_key_words
 from flask import Flask, render_template
 from dotenv import load_dotenv
 from flaskr.config import DevelopmentConfig
@@ -9,6 +9,7 @@ env_path = os.path.join(os.getcwd(), 'env')
 load_dotenv(env_path)
 
 # Working with PDFs in FLASK: https://www.geeksforgeeks.org/working-with-pdf-files-in-python/
+# FIXME: Have configuration work automatically from environment variables instead of having to run 'set FLASK_APP=flaskr' every time
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(DevelopmentConfig)
@@ -20,7 +21,8 @@ resume_path = os.path.join(parent_dir, "resume.pdf")
 @app.route("/")
 def hello_world():
     pdf_text = extract_text(resume_path)
-    return render_template("index.html", pdf_text=pdf_text)
+    keywords = extract_key_words(pdf_text)
+    return render_template("index.html", keywords=keywords)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- flaskr/pdf.py
+++ flaskr/pdf.py
@@ -1,7 +1,11 @@
 import PyPDF2
+import spacy
 import os
 
 # FIXME: Create a folder that holds all of the Resumes. Will later turn that into an SQL Database
+
+# https://towardsdatascience.com/keyword-extraction-process-in-python-with-natural-language-processing-nlp-d769a9069d5c
+# spacy looks like a good option, I am not going to build my own NLP model for this project, I would rather just use a pre-built one, or multiple pre-built ones and cross reference their results21
 
 def extract_text(pdf):
     """Method that accepts a pdf file/path to pdf file as its argument
@@ -11,3 +15,9 @@ def extract_text(pdf):
     pdf_reader = PyPDF2.PdfFileReader(pdf)
     page = pdf_reader.getPage(0) # Should always be one page since only accepting 1 page resumes
     return page.extractText()
+
+def extract_key_words(description):
+    """Accepts a string and uses spacy to return the keywords"""
+    nlp = spacy.load("en_core_web_md")
+    doc = nlp(description)
+    return doc.ents
