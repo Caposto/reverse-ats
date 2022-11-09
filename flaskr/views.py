49602a903110a8c4d60d38daadbed7ebe1445111
ChristianA <christiand.apostol@outlook.com>
2022-11-09 17:10:51 -0500

    removed models

--- flaskr/views.py
+++ flaskr/views.py
@@ -2,6 +2,7 @@
 # FIXME: Request should recieve text and respond with JSON of keywords 
 
 from flask import Blueprint, jsonify
+from flaskr.pdf import extract_text, extract_key_words
 # Blueprint documentation: https://flask.palletsprojects.com/en/2.2.x/tutorial/views/
 
 main = Blueprint('main', __name__)
