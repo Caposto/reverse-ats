e77dfa42ad017bff0fa6675fd59a43e40914594e
ChristianA <christiand.apostol@outlook.com>
2022-11-10 18:57:26 -0500

    display keywords on React page

--- frontend/src/components/JobForm.js
+++ frontend/src/components/JobForm.js
@@ -1,37 +1,43 @@
 import React, {useState} from 'react';
-import { Button, Form, Message, Input } from 'semantic-ui-react';
+import { Button, Form, Input } from 'semantic-ui-react';
 
 export const JobForm = () => {
     const [description, setDescription] = useState(''); // Create state for job description, default empty string
+    const [keywords, setKeywords] = useState([]); // Create state for keywords received from server
 
     // Return form for inputting job description
     return (
+        <div>
+            <Form> 
+                <Form.Field>
+                    <Input
+                      placeholder="job description" 
+                      value={description} 
+                      onChange={e => setDescription(e.target.value)}  
+                    />
+                </Form.Field>
+                <Button onClick={async() => {
+                    const job_description = {description};
+                    const response = await fetch('/extract_keywords', {
+                        method: 'POST', // Using POST since GET does not accept a body
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(job_description)
+                    });
 
-        <Form> 
-            <Form.Field>
-                <Input
-                  placeholder="job description" 
-                  value={description} 
-                  onChange={e => setDescription(e.target.value)}  
-                />
-            </Form.Field>
-            <Button onClick={async() => {
-                const job_description = {description};
-                const response = await fetch('/extract_keywords', {
-                    method: 'POST', // Using POST since GET does not accept a body
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(job_description)
-                });
+                    if (response.ok) {
+                        let res = response.json();
+                        res.then((prom) => setKeywords(prom))
+                        console.log(res);
+                        setDescription(''); // Clear the form
+                    }
 
-                if (response.ok) {
-                    let res = response.json();
-                    console.log(res);
-                    setDescription(''); // Clear the form
-                }
-                
-            }}>Submit</Button>
-        </Form>
+                }}>Submit</Button>
+            </Form>
+            <p>
+                {keywords}
+            </p>
+        </div>
     )
 }
