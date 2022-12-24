d14955aba02ea66a39de9a3fb162a019bc7bcdf8
ChristianA <christiand.apostol@outlook.com>
2022-12-24 11:30:44 -0500

    space filtering on job description

--- flaskr/extraction.py
+++ flaskr/extraction.py
@@ -1,5 +1,6 @@
 import PyPDF2
 import spacy
+import re
 
 # Working with PDFs in FLASK: https://www.geeksforgeeks.org/working-with-pdf-files-in-python/
 # https://towardsdatascience.com/keyword-extraction-process-in-python-with-natural-language-processing-nlp-d769a9069d5c
@@ -20,6 +21,7 @@ def extract_keywords_md(description: str) -> list[str]:
     nlp = spacy.load("en_core_web_md")
     doc = nlp(description)
     keyword_list = [word.text for word in doc.ents] # Get each keyword as a string
+    filtered_keyword_list = [w for w in keyword_list if " " not in w]
     return keyword_list
 
 def extract_keywords_sm(description: str) -> list[str]:
@@ -43,4 +45,4 @@ def extract_keywords_trf(description: str) -> list[str]:
     keyword_list = [word.text for word in doc.ents] # Get each keyword as a string
     return keyword_list
 
-# Uses keyBERT
+# Uses Keybert
--- flaskr/tests/extract_test.py
+++ flaskr/tests/extract_test.py
@@ -1,3 +1,6 @@
+# TODO: Correctly resolve imports for extraction methods https://www.geeksforgeeks.org/python-import-from-parent-directory/
+# TODO: Format text to get rid of newlines and stuff
+
 def test_extraction(function):
     # Read description from text file
     with open('description.txt', 'r') as f:
