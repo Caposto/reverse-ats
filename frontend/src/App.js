fbd806b1dc087e26270ae5d0bd195d676c33bdcd
ChristianA <christiand.apostol@outlook.com>
2023-02-01 13:10:10 -0500

    basic 2 view implementation using a bool

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-// import { Container } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import './App.css';
-// import { JobForm } from './components/JobForm';
+import { JobForm } from './components/JobForm';
 import KeywordList2 from './components/KeywordList2';
 
 function App() {
-  return (
-    <div className='keyword-app'>
-      <KeywordList2/>
-    </div>
-    
-    /* Current Basic Reverse-ATS
-    <div className="App">
-      <Container>
-        <JobForm />
-      </Container>
-    </div>
-    */
-  );
+  let home = true;
+  
+  if (home) {
+    return (
+      <div className='keyword-app'>
+        <KeywordList2/>
+      </div> )
+  } else {
+    return (
+      <div className="App">
+        <Container>
+          <JobForm />
+        </Container>
+      </div>
+    )
+  }
 }
 
 export default App;
