1618ff91c7e82d045106f154187f5d5f6ab618d3
ChristianA <christiand.apostol@outlook.com>
2023-02-05 16:13:27 -0500

    implemented API request via basic form

--- frontend/src/components/JobForm.js
+++ frontend/src/components/JobForm.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import { Button, Form, Input } from 'semantic-ui-react';
 import KeywordList from './KeywordList'; // Component to display keywords
 
 export const JobForm = () => {
@@ -7,45 +6,44 @@ export const JobForm = () => {
     const [keywords, setKeywords] = useState([]); // Create state for keywords received from server
 
     // TODO: Edit state of list with the intial list being the keywords processed from flask
-    const [list, setList] = useState(keywords); // Pass the state variable the keywords as the "inital list"
 
-    function handleRemove() {
-        console.log("Deleted Keyword!");
+    let handleSubmit = async (e) => {
+        e.preventDefault(); // Prevent page from refreshing
+
+        try {
+            // TODO: store url for server in a .env file
+            const response = await fetch('http://127.0.0.1:5000/extract_keywords', {
+                method: 'POST', // Using POST since GET does not accept a body
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({description})
+            });
+
+            if (response.ok) {
+                let res = response.json();
+                res.then((prom) => setKeywords(prom)); // Store keywords in {keywords}
+                setDescription(''); // Clear the form
+            }
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     // Return form for inputting job description
     return (
-        <div>
-            <Form>
-                <Form.Field>
-                    <Input
-                      placeholder="job description" 
-                      value={description} 
-                      onChange={e => setDescription(e.target.value)}  
-                    />
-                </Form.Field>
-                <Button onClick={async() => {
-                    const job_description = {description};
+        <div id="standard-form">
+            <form onSubmit={handleSubmit}>
+                <input 
+                    placeholder='Paste Job Description'
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                />
+                <button type="submit">Submit</button>
+            </form>
 
-                    // TODO: store url for server in a .env file
-                    const response = await fetch('http://127.0.0.1:5000/extract_keywords', {
-                        method: 'POST', // Using POST since GET does not accept a body
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(job_description)
-                    });
-
-                    if (response.ok) {
-                        let res = response.json();
-                        res.then((prom) => setKeywords(prom)); // Store keywords in {keywords}
-                        setDescription(''); // Clear the form
-                    }
-
-                }}>Submit</Button>
-            </Form>
-            <div>
-                <KeywordList keywordArray = {keywords}/>
+            <div id="keyword-list">
+                <KeywordList keywordArray={keywords}/>
             </div>
         </div>
     )
