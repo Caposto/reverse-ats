c6a963d35d4fc9d55c56a3cc3205116d7952ae61
ChristianA <christiand.apostol@outlook.com>
2023-02-12 16:50:35 -0500

    single page implmentation on JobForm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
-import EntryForm from './routes';
+import EntryForm from './routes/index';
 import NotFound from './routes/error';
 import CurrentKeywords from './routes/list';
-
 
 function App() {
   return (
--- frontend/src/components/JobForm.js
+++ frontend/src/components/JobForm.js
@@ -1,9 +1,11 @@
 import {useState} from 'react';
+import KeywordList2 from './KeywordList2';
 import KeywordList from './KeywordList'; // Component to display keywords
 
 export const JobForm = () => {
     const [description, setDescription] = useState(''); // Create state for job description, default empty string
     const [keywords, setKeywords] = useState([]); // Create state for keywords received from server
+    const [visible, setVisibility] = useState(true); // Hide form once submitted
 
     // TODO: Edit state of list with the intial list being the keywords processed from flask
 
@@ -24,6 +26,7 @@ export const JobForm = () => {
                 let res = response.json();
                 res.then((prom) => setKeywords(prom)); // Store keywords in {keywords}
                 setDescription(''); // Clear the form
+                setVisibility(false);
             }
         } catch (err) {
             console.log(err);
@@ -33,18 +36,21 @@ export const JobForm = () => {
     // Return form for inputting job description/
     return (
         <div id="standard-form">
-            <form onSubmit={handleSubmit}>
-                <input 
-                    placeholder='Paste Job Description'
-                    value={description}
-                    onChange={e => setDescription(e.target.value)}
-                />
-                <button type="submit">Submit</button>
-            </form>
-
-            <div id="keyword-list">
-                <KeywordList keywordArray={keywords}/>
-            </div>
+            {visible && (
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        placeholder='Paste Job Description'
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
+                    <button type="submit">Submit</button>
+                </form> 
+            )}
+            {!visible && (
+                <div className='keyword-app'>
+                    <KeywordList2/>
+                </div> 
+            )}
         </div>
     )
 }
