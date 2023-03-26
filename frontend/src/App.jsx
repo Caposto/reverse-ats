a2f7596024867dedb1fb497d0d67984aeb422253
Caposto <christiand.apostol@outlook.com>
2023-03-25 22:49:18 -0400

    removed react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,8 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import NotFound from "./components/NotFound";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
+  return <Home />;
 }
 
 export default App;
