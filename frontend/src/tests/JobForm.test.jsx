0be8097055cea039901e19468eae771d3d8c44ef
ChristianA <christiand.apostol@outlook.com>
2023-03-30 14:55:02 -0400

    made first snapshot test in react

---
+++ frontend/src/tests/JobForm.test.jsx
@@ -0,0 +1,24 @@
+import renderer from "react-test-renderer";
+import JobForm from "../components/JobForm";
+
+it("changes the class when hovered", () => {
+  const component = renderer.create(<JobForm />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  renderer.act(() => {
+    tree.props.onMouseEnter();
+  });
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  renderer.act(() => {
+    tree.props.onMouseLeave();
+  });
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
