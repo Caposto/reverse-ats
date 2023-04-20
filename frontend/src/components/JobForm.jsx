c90c0e7259e84be12411ab5565e925908bb42d25
Caposto <christiand.apostol@outlook.com>
2023-04-19 20:39:21 -0400

    hide form after submitting

--- frontend/src/components/JobForm.jsx
+++ frontend/src/components/JobForm.jsx
@@ -11,6 +11,7 @@ function JobForm() {
   const [count, setCount] = useState(0);
   const [error, setError] = useState(false);
   const [exception, setException] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
 
   const maxJobDescriptionLength = 5000;
 
@@ -31,6 +32,7 @@ function JobForm() {
         setCount(0);
         setVisibility(false);
         setLoading(false);
+        setSubmitted(true);
       } catch (err) {
         setLoading(false);
         // eslint-disable-next-line no-console
@@ -40,41 +42,49 @@ function JobForm() {
     }
   };
 
+  const submitNewDescription = () => {
+    setSubmitted(false);
+    window.location.reload();
+  };
+
   return (
     <div aria-label="form-test-id" className="p-8">
-      <form onSubmit={handleSubmit}>
-        <textarea
-          aria-label="Job Description Submission"
-          placeholder="Enter Job Description"
-          value={description}
-          onChange={(e) => {
-            setDescription(e.target.value);
-            setCount(e.target.value.length);
-          }}
-          rows="6"
-          maxLength={maxJobDescriptionLength}
-          className="block px-4 w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
-        />
-        <div className="pt-2">
-          Character Limit: {count}/{maxJobDescriptionLength}
-        </div>
-        <div className="py-4">
-          <button
-            className="text-xl p-2 rounded-md border border-2"
-            type="submit"
-            aria-label="Submit Button"
-          >
-            {loading ? <>Loading..</> : <>Submit</>}
-          </button>
-        </div>
-      </form>
+      {!submitted && (
+        <form onSubmit={handleSubmit}>
+          <textarea
+            aria-label="Job Description Submission"
+            placeholder="Enter Job Description"
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+              setCount(e.target.value.length);
+            }}
+            rows="6"
+            maxLength={maxJobDescriptionLength}
+            className="block px-4 w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
+          />
+          <div className="pt-2">
+            Character Limit: {count}/{maxJobDescriptionLength}
+          </div>
+          <div className="py-4">
+            <button
+              className="text-xl p-2 rounded-md border border-2"
+              type="submit"
+              aria-label="Submit Button"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      )}
+      <div className="text-xl"> {loading ? <>Loading...</> : ""} </div>
       {!visible && !error && !exception && (
         <div>
           <KeywordList initial={keywords} />
           <button
             className="text-xl p-2 rounded-md border border-2"
             type="submit"
-            onClick={() => window.location.reload()} // FIXME: Is there a way to reload just the component instead of whole page?
+            onClick={submitNewDescription} // FIXME: Is there a way to reload just the component instead of whole page?
           >
             Submit New Description
           </button>
