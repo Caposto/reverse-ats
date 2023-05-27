6127afc652627d4ad4cde757a7c0cca04baf0b6d
ChristianA <christiand.apostol@outlook.com>
2023-05-26 22:39:18 -0400

    tentative progress bar redo

--- frontend/src/components/CircularProgressBar.jsx
+++ frontend/src/components/CircularProgressBar.jsx
@@ -1,35 +1,14 @@
 import PropTypes from "prop-types";
 
 function CircularProgressBar({ percentage }) {
-  const circumference = 2 * Math.PI * 50; // circumference of a circle with a radius of 50
-
-  const strokeDashoffset = circumference - (percentage / 100) * circumference;
-
-  // FIXME: Percentage not centered
   return (
-    <div className="relative w-32 h-32">
-      <svg
-        className="absolute top-0 left-0"
-        viewBox="0 0 100 100"
-        width="100"
-        height="100"
-      >
-        <circle cx="50" cy="50" r="40" fill="none" stroke="#e5e7eb" strokeWidth="10" />
-        <circle
-          cx="50"
-          cy="50"
-          r="40"
-          fill="none"
-          stroke="#4f46e5"
-          strokeWidth="10"
-          strokeLinecap="round"
-          strokeDasharray={circumference}
-          strokeDashoffset={strokeDashoffset}
-        />
-      </svg>
-      <span className="absolute inset-0 flex items-center justify-center text-lg font-bold">
-        {percentage}%
-      </span>
+    <div className="progress-bar relative w-24 h-24 rounded-full overflow-hidden">
+      <div className="absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-r from-hotpink via-pink to-transparent transform rotate-180" />
+      <progress
+        className="absolute top-0 right-0 bottom-0 left-0 w-full h-full appearance-none bg-transparent"
+        value={percentage}
+        max="100"
+      />
     </div>
   );
 }
