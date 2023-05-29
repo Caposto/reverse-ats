5474b79a7905fde587e8be6d4e2c07ac4312a0b5
ChristianA <christiand.apostol@outlook.com>
2023-05-29 16:07:09 -0400

    load spacy model once

--- server/application/api.py
+++ server/application/api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request, abort
 from json import dumps
 from marshmallow import Schema, fields, ValidationError
+from spacy import load
 from .extraction import extract_keywords_md
 
 main = Blueprint('main', __name__)
@@ -8,6 +9,9 @@ main = Blueprint('main', __name__)
 # Schema accepts a single field: string
 class RequestSchema(Schema):
     description = fields.String(required=True)
+
+# Load the spacy model
+nlp = load("en_core_web_md")
 
 # API Endpoint
 @main.route('/extract_keywords', methods=['POST'])
@@ -21,10 +25,10 @@ def get_keywords():
         try:
             result = schema.load(request_body)
         except ValidationError as err:
-            return jsonify(err.message, 200)
+            return jsonify(err.message, 400)
 
         job_description_text = dumps(result)
-        keywords = extract_keywords_md(job_description_text) # list/array : extraction.py
+        keywords = extract_keywords_md(job_description_text, nlp) # list/array : extraction.py
         return jsonify(keywords), 200
     else:
-        abort(405) # Exit with status code 405 : 
+        abort(405) # Exit with status code 405 : Method not allowed
--- server/application/extraction.py
+++ server/application/extraction.py
@@ -1,27 +1,13 @@
-import PyPDF2
-import spacy
 import re
+from spacy import load
 
 #################################################SPACY##############################################################
 
-# FIXME: What is the output? A string?
-def extract_pdf_text(pdf: str):
-    """Method that accepts a pdf file/path to pdf file as its argument
-       and uses PyPDF2 to scrape and then return a dictionary of keywords"""
-    pdf = open(pdf, 'rb')
-    pdf_reader = PyPDF2.PdfFileReader(pdf)
-    page = pdf_reader.getPage(0) # Should always be one page since only accepting 1 page resumes
-    return page.extractText()
-
-#FIXME: Look into: https://spacy.io/models to see if there are ways to optimize the loading process by disabling certain features
-
-def extract_keywords_md(description: str) -> list[str]:
+def extract_keywords_md(description: str, nlp_model = load("en_core_web_md")) -> list[str]:
     """Accepts a string and uses spacy to return the keywords as a list of strings using en_core_web_md"""
-    nlp = spacy.load("en_core_web_md")
-    doc = nlp(description)
+    doc = nlp_model(description)
     keyword_list = [word.text for word in doc.ents] # Get each keyword as a string
-    filtered_keyword_list = [w for w in keyword_list if " " not in w]
-    filtered_keyword_list = [*set(filtered_keyword_list)] # Remove duplicates using a hash set
+    filtered_keyword_list = [*set(keyword_list)] # Remove duplicates using a hash set
     return filtered_keyword_list
 
 def extract_re(description: str) -> list[str]:
