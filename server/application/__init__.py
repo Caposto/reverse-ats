3a3cfe542e9975999fb2cde61c137769abdf695c
ChristianA <christiand.apostol@outlook.com>
2023-06-02 17:46:27 -0400

    updated production config

--- server/application/__init__.py
+++ server/application/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 from flask_cors import CORS
-from config import DevelopmentConfig
+from config import ProductionConfig
 
 env_path = os.path.join(os.getcwd(), 'env')
 load_dotenv(env_path)
@@ -10,7 +10,7 @@ load_dotenv(env_path)
 def init_app():
   """Initialize the core application"""
   app = Flask(__name__, instance_relative_config=True)
-  app.config.from_object(DevelopmentConfig) # Configure app from config.py
+  app.config.from_object(ProductionConfig) # Configure app from config.py
   cors = CORS(app)
   app.config['CORS_HEADERS'] = 'Content-Type'
 
