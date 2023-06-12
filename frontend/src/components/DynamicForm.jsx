cff302a7e9d51e7e97e4b61db645ad3b7b89658e
Caposto <christiand.apostol@outlook.com>
2023-06-12 19:07:26 -0400

    final

--- frontend/src/components/DynamicForm.jsx
+++ frontend/src/components/DynamicForm.jsx
@@ -83,13 +83,13 @@ function DynamicForm({ descriptionType }) {
             maxLength={maxJobDescriptionLength}
             className="block px-4 w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
           />
-          <div className="flex items-center">
+          <div className="flex items-center border-2">
             <div className="p-4">
               <button className="button-sm" type="submit" aria-label="Submit Button">
                 Extract
               </button>
             </div>
-            <div className="pt-2">
+            <div>
               Character Limit: {count}/{maxJobDescriptionLength}
             </div>
             <div className="text-xl pl-2"> {loading ? <Loader /> : ""} </div>
