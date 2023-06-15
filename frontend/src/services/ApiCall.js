866a4cdbfe8133619e261b051d0a2287672e226e
Caposto <christiand.apostol@outlook.com>
2023-06-15 18:36:13 -0400

    timeout on api

--- frontend/src/services/ApiCall.js
+++ frontend/src/services/ApiCall.js
@@ -1,20 +1,29 @@
 async function getKeywordsFromFlask(description) {
-  const response = await fetch("https://reverse-ats-api.onrender.com/extract_keywords", {
-    method: "POST", // Using POST since GET doesn't accept strings greater than 2000 characters
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ description }),
-  });
+  try {
+    const fetchPromise = fetch("https://reverse-ats-api.onrender.com/extract_keywords", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ description }),
+    });
 
-  const responseIsSuccessful = response.ok;
-  let data;
+    const timeoutPromise = new Promise(
+      // eslint-disable-next-line no-promise-executor-return
+      (_, reject) => setTimeout(() => reject(new Error("Request timed out")), 10000) // 10 seconds
+    );
 
-  if (responseIsSuccessful) {
-    data = await response.json();
+    const response = await Promise.race([fetchPromise, timeoutPromise]);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return null;
   }
-
-  return data;
 }
 
 export default getKeywordsFromFlask;
